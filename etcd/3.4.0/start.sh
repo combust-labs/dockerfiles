70df714b1a53cd275549e34487929e59f8d6cc5d
Radek Gruchalski <radek@gruchalski.com>
2021-03-17 01:01:40 +0000

    Fix etcd basic service file for 3.4.0

--- etcd/3.4.0/start.sh
+++ etcd/3.4.0/start.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-# check for $CLIENT_URLS
+self_ip=`ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+
+if [ -z ${ETCD_NAME+x} ]; then
+    ETCD_NAME="${self_ip}"
+    echo "Using default ETCD_NAME ($ETCD_NAME)"
+else
+    echo "Detected new ETCD_NAME value of $ETCD_NAME"
+fi
+
 if [ -z ${CLIENT_URLS+x} ]; then
     CLIENT_URLS="http://0.0.0.0:4001,http://0.0.0.0:2379"
     echo "Using default CLIENT_URLS ($CLIENT_URLS)"
 else
     echo "Detected new CLIENT_URLS value of $CLIENT_URLS"
 fi
-
-# check for $PEER_URLS
 
 if [ -z ${PEER_URLS+x} ]; then
     PEER_URLS="http://0.0.0.0:7001,http://0.0.0.0:2380"
@@ -17,7 +23,36 @@ else
     echo "Detected new PEER_URLS value of $PEER_URLS"
 fi
 
-ETCD_CMD="/bin/etcd -data-dir=/data -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} $*"
+if [ -z ${ADVERTISE_CLIENT_URLS+x} ]; then
+    ADVERTISE_CLIENT_URLS="http://${self_ip}:2379"
+    echo "Using default ADVERTISE_CLIENT_URLS ($ADVERTISE_CLIENT_URLS)"
+else
+    echo "Detected new ADVERTISE_CLIENT_URLS value of $ADVERTISE_CLIENT_URLS"
+fi
+
+if [ -z ${ADVERTISE_PEER_URLS+x} ]; then
+    ADVERTISE_PEER_URLS="http://${self_ip}:2380"
+    echo "Using default ADVERTISE_PEER_URLS ($ADVERTISE_PEER_URLS)"
+else
+    echo "Detected new ADVERTISE_PEER_URLS value of $ADVERTISE_PEER_URLS"
+fi
+
+if [ -z ${INITIAL_CLUSTER+x} ]; then
+    INITIAL_CLUSTER="${self_ip}=http://${self_ip}:2380"
+    echo "Using default INITIAL_CLUSTER ($INITIAL_CLUSTER)"
+else
+    echo "Detected new INITIAL_CLUSTER value of $INITIAL_CLUSTER"
+fi
+
+ETCD_CMD="/bin/etcd --logger zap --data-dir /data "
+ETCD_CMD="$ETCD_CMD --name ${ETCD_NAME}"
+ETCD_CMD="$ETCD_CMD --listen-peer-urls ${PEER_URLS}"
+ETCD_CMD="$ETCD_CMD --listen-client-urls ${CLIENT_URLS}"
+ETCD_CMD="$ETCD_CMD --initial-cluster ${INITIAL_CLUSTER}"
+ETCD_CMD="$ETCD_CMD --initial-cluster-state new"
+ETCD_CMD="$ETCD_CMD --initial-advertise-peer-urls ${ADVERTISE_PEER_URLS}"
+ETCD_CMD="$ETCD_CMD --advertise-client-urls ${ADVERTISE_CLIENT_URLS} $*"
+
 echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
 
 exec $ETCD_CMD
